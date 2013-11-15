b1c26273c6d03ab085834b11d52e0f289737a06a
Zachary Parmley <zparmley@140-sfoengwifi54-205.clients.corp.yelpcorp.com>
2013-11-15 11:19:34 -0800

    Neqly

--- wallet/connection.py
+++ wallet/connection.py
@@ -8,3 +8,20 @@ def get_bitcoin_connection():
         config.host,
         config.port
     )
+
+def get_source_connection():
+    return BitcoinConnection(
+        config.rpcuser,
+        config.rpcpassword,
+        config.source_host,
+        config.source_port
+    )
+
+
+def get_mek_connection(host):
+    return BitcoinConnection(
+        config.rpcuser,
+        config.rpcpassword,
+        host,
+        config.source_port
+    )
