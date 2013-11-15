6ab2115d700cbd2f060488a07827e5651026f78d
Zachary Parmley <zparmley@140-sfoeng50-190.clients.corp.yelpcorp.com>
2013-11-14 16:50:56 -0800

    Adding conf

---
+++ config/votes.py
@@ -0,0 +1,10 @@
+votes = {
+    'yea': {
+        'address': 'msheGGqfR2wYCTUxCbi2zc5jMAeyFySYfz',
+        'label': 'Vote%20Yes',
+        'ammount': .001,
+    }, 'nay': {
+        'address': 'n2ZtCWod8yYSD7fZzs9NyqyiehQZU62kbg',
+        'label': 'Vote%20No',
+        'ammount': .001,
+    }}
